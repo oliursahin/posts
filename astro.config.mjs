ebd007fb6f71710348a45174239a69cf562e336f
sajdkabir <sajda.kbir@gmail.com>
2024-08-13 15:35:14 +0530

    feat: newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from "astro/config";
+import { defineConfig , envField} from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  experimental: {
+    env: {
+        schema: {
+            NEWSLETTER_LOOPS_API: envField.string({ context: "server", access: "secret" }),
+        }
+    }
+},
 });
